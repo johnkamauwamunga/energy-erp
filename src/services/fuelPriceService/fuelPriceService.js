48622e5a1a9c24d9c00126e623a4a2187b23fc1c
johnkamauwamunga <johnkamauwamunga@gmail.com>
2025-10-18 07:38:54 +0300

    updated fuel pricing1

--- src/services/fuelPriceService/fuelPriceService.js
+++ src/services/fuelPriceService/fuelPriceService.js
@@ -545,7 +545,6 @@ class FuelPriceService {
           baseCostPrice: product.baseCostPrice * (1 + percentage / 100),
           minSellingPrice: product.minSellingPrice * (1 + percentage / 100),
           maxSellingPrice: product.maxSellingPrice * (1 + percentage / 100)
-          
         }));
       
       if (updates.length === 0) {
